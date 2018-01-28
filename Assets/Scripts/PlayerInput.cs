7af9e42eaedd21c2b9721cd08f916adc29464288
Paul Hansa <bigcooki3@users.noreply.github.com>
2018-01-27 23:52:36 -0500

    Can now switch between objects with click

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayerInput : MonoBehaviour
+{
+    public bool isActive;
+    Player player;
+    GameObject[] players;
+    private void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        player = GetComponent<Player>();
+    }
+    private void Update()
+    {
+        if (isActive)
+        {
+            Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            player.SetDirectionalInput(directionalInput);
+            if (Input.GetButtonDown("Fire1"))
+            {
+                player.OnJumpInputDown();
+            }
+            if (Input.GetButtonUp("Fire1"))
+            {
+                player.OnJumpInputUp();
+            }
+            if (Input.GetButtonUp("Fire2"))
+            {
+                isActive = true;
+            }
+        }
+    }
+    void OnMouseDown()
+    {
+        foreach (GameObject gameObject in players)
+        {
+            gameObject.GetComponent<PlayerInput>().isActive = false;
+            gameObject.GetComponent<Player>().directionalInput = Vector2.zero;
+        }
+        isActive = true;
+    }
+}
