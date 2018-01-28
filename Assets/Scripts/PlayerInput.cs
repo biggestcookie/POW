0270cbe8915aaf89a2157d62202df547de2488f2
Paul Hansa <bigcooki3@users.noreply.github.com>
2018-01-28 14:32:16 -0500

    idk

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(Player))]
 public class PlayerInput : MonoBehaviour
 {
+    static Vector2 FirstPosition;
     public bool isActive;
     Player player;
     PlayerInput input;
     GameObject[] playerList;
+    //public GameObject lightning;
     new GameObject camera;
     Light lighter;
     private void Start()
@@ -17,6 +19,7 @@ public class PlayerInput : MonoBehaviour
         camera = GameObject.FindGameObjectWithTag("MainCamera");
         player = GetComponent<Player>();
         lighter = gameObject.GetComponentInChildren(typeof(Light)) as Light;
+        //lightning = GameObject.FindGameObjectWithTag("Lightning");
     }
     private void Update()
     {
@@ -59,12 +62,44 @@ public class PlayerInput : MonoBehaviour
         camera.GetComponent<Tracker>().active = gameObject;
         AddLight();
         isActive = true;
+        /*float SizeScale = 1.0f;
+        Vector2 NewSize = new Vector2();
+        // SpriteRenderer lightningRenderer = lightning.GetComponent<SpriteRenderer>();
+        print(Vector3.Distance(FirstPosition, transform.position) / SizeScale);
+        NewSize.Set(Vector3.Distance(transform.position, FirstPosition) / SizeScale, lightningRenderer.size.y);
+        //lightningRenderer.size = NewSize;
+
+        // Set Rotation
+        //lightning.transform.rotation = Quaternion.Euler(0, 0, 0);
+        //lightning.transform.Rotate(0, 0, AngleBetweenVector2(FirstPosition, transform.position));
+
+        // Set Position
+        float posOffset = 0.0f;
+        if (transform.position.x >= FirstPosition.x)
+        {
+            posOffset = lightning.GetComponent<Renderer>().bounds.size.x / 2;
+        }
+        else if (transform.position.x < FirstPosition.x)
+        {
+            posOffset = -(lightning.GetComponent<Renderer>().bounds.size.x / 2);
+        }
+        Vector2 VectorOffset = new Vector2();
+        VectorOffset.Set(posOffset, 0);
+        lightning.transform.position = FirstPosition + VectorOffset;
+
+        FirstPosition = transform.position;*/
     }
-    void AddLight()
+    /*void AddLight()
     {
         if (lighter != null)
         {
             lighter.intensity = 3;
         }
     }
+    private float AngleBetweenVector2(Vector2 vec1, Vector2 vec2)
+    {
+        Vector2 diference = vec2 - vec1;
+        float sign = (vec2.y < vec1.y) ? -1.0f : 1.0f;
+        return Vector2.Angle(Vector2.right, diference) * sign;
+    }*/
 }
--- Assets/Scripts/effect.cs
+++ Assets/Scripts/effect.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class effect : MonoBehaviour {
+public class effect : MonoBehaviour
+{
 
     // Use this for initialization
-    void Start () {
-        if (this.gameObject.CompareTag("effect"))
+    void Start()
+    {
+        if (this.gameObject.CompareTag("Lightning"))
         {
             Animation Vfx = this.gameObject.GetComponent<Animation>();
             Vfx.Play();
             Destroy(this.gameObject, Vfx.clip.length);
         }
     }
-    
+
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
     }
 }
