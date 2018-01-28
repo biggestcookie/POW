aefbff63afd6d8cc4915fda85f424c9f065b1fab
Paul Hansa <bigcooki3@users.noreply.github.com>
2018-01-27 23:04:18 -0500

    Revert "Commit before merge with /daniel"
    This reverts commit 9fd0948deb969a1aec9a9816176d581725107228.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,90 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
-    public float maxJumpHeight;
-    public float minJumpHeight;
-    public float timeToJumpApex;
-    public float moveSpeed;
-    public float fireRate;
 
-    float nextFire;
-    float gravity;
-    float maxJumpVelocity;
-    float minJumpVelocity;
-    float velocityXSmoothing;
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
     float accelerationTimeAirborne = .2f;
     float accelerationTimeGrounded = .1f;
-    public bool direction;
-    private Animator animator;
-    Vector2 directionalInput;
+    float moveSpeed = 6;
+
+    float gravity;
+    float jumpVelocity;
     Vector3 velocity;
+    float velocityXSmoothing;
 
-    Controller2D controller2D;
-    private void Start()
+    Controller2D controller;
+
+    void Start()
     {
-        controller2D = GetComponent<Controller2D>();
-        gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
-        direction = true;
-        //animator = GetComponent<Animator>();
+        controller = GetComponent<Controller2D>();
 
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        print("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
     }
+
     void Update()
     {
-        CalculateVelocity();
-        controller2D.Move(velocity * Time.deltaTime);
-        if (controller2D.collisions.above || controller2D.collisions.below)
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
             velocity.y = 0;
-        if ((velocity.x > 0 && !direction) || (velocity.x < 0 && direction))
-            FlipSprite();
-        //animator.SetBool("inAir", !controller2D.collisions.below);
-        //animator.SetFloat("speed", Mathf.Abs(velocity.x));
-    }
-    public void SetDirectionalInput(Vector2 input)
-    {
-        directionalInput = input;
-    }
+        }
 
-    public void OnJumpInputDown()
-    {
-        if (controller2D.collisions.below)
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
         {
-            velocity.y = maxJumpVelocity;
+            velocity.y = jumpVelocity;
         }
-    }
-    public void OnJumpInputUp()
-    {
-        if (velocity.y > minJumpVelocity)
-        {
-            velocity.y = minJumpVelocity;
-        }
-    }
-    /*public void OnMousePress()
-    {
-        if (Time.time > nextFire)
-        {
-            nextFire = Time.time + fireRate;
-                Instantiate(shot, gun.position, gun.rotation);
-            //GetComponent<AudioSource>().Play();
-        }
-    }*/
-    private void FlipSprite()
-    {
-        direction = !direction;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
-    void CalculateVelocity()
-    {
-        float targetVelocityX = directionalInput.x * moveSpeed;
-        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, controller2D.collisions.below ? accelerationTimeGrounded : accelerationTimeAirborne);
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
         velocity.y += gravity * Time.deltaTime;
-
+        controller.Move(velocity * Time.deltaTime);
     }
 }
