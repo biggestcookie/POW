ecace71746220e32899eb4beab2e558181d9aa54
Paul Hansa <bigcooki3@users.noreply.github.com>
2018-01-27 19:01:31 -0500

    Unity commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# POW-game
-A game made in Godot Engine 3.0 for Global Game Jam 2018
--- Template/coin.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-var taken=false
-
-func _on_coin_body_enter( body ):
-	if not taken and body is preload("res://Template/player.gd"):
-		$anim.play("taken")
-		taken = true
